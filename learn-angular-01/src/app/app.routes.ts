c46d02bf1def5149182ca38830c9f3ffa58dc79f
Hoang Van Tri <trihoangvan16092003@gmail.com>
2025-04-09 16:06:10 +0700

    refactor(app.routes): replace direct component imports with lazy loading
    Implement lazy loading for the 'detail' and 'create' components to reduce initial load time and memory usage. This change improves performance by loading components only when needed.

--- learn-angular-01/src/app/app.routes.ts
+++ learn-angular-01/src/app/app.routes.ts
@@ -6,6 +6,19 @@ import { CreateComponent } from './create/create.component';
 export const routes: Routes = [
     //  Trang chính ( path trống thì mậc định là trang chủ)
   { path: '', component: HomeComponent} ,
-  { path: 'detail/:id', component: DetailComponent},
-  { path: 'create', component: CreateComponent}
+  // { path: 'detail/:id', component: DetailComponent},
+  // { path: 'create', component: CreateComponent}
+
+  // Áp dung Lazy Loading với các component khác
+  // Khi vào trang detail thì sẽ load component detail vào
+  // Khi vào trang create thì sẽ load component create vào 
+  // Giúp tiết kiệm dung lượng bộ nhớ khi load trang
+  {
+    path: 'detail/:id',
+    loadComponent: () => import('./detail/detail.component').then(m => m.DetailComponent)
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./create/create.component').then(m => m.CreateComponent)
+  }
 ];
